ab2ea7e4ccd89a5a535a356a700cb0615662d551
Andrzejowski Grzegorz <grzegorz.andrzejowski@gmail.com>
2020-03-22 19:24:36 +0100

    Create FieldLabel class and create some example labels

--- src/Components/App.ts
+++ src/Components/App.ts
@@ -1,8 +1,16 @@
 import Component from "../Interfaces/Component";
+import FieldLabel  from './FieldLabel';
 
 const App = (): Component => ({
     render(parent: HTMLElement): void {
         parent.innerHTML = "Hello from App.ts";
+        parent.innerHTML += "<hr /> Here are some labels:";
+        const labels = document.createElement('div');
+        const label1: FieldLabel = new FieldLabel('First label');
+        const label2: FieldLabel = new FieldLabel('Second Label');
+        label1.render(labels);
+        label2.render(labels);
+        parent.appendChild(labels);
     }
 });
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import App from './Components/App';
 
 const rootElement: HTMLElement | null = document.getElementById('appRoot');
-console.log(`Root Element: ${rootElement}`);
 
 rootElement && App.render(rootElement);
